6e6a5db3540d72f088b9e92963b255d8caef91b4
Andrew Ziegler <14apdjz@gmail.com>
2022-02-26 10:36:50 -0500

    Create Prototype.js

---
+++ Prototype.js
@@ -0,0 +1,15 @@
+class Movie {
+  constructor(title, director, genre, releaseYear, rating) {
+    this.title = title;
+    this.director = director;
+    this.genre = genre;
+    this.releaseYear = releaseYear;
+    this.rating = rating;
+  }
+
+  getOverview() {
+    return `${this.title}, a ${this.genre} film directed by ${this.director} was released in ${this.releaseYear}. It received a rating of ${this.rating}`;
+  }
+}
+
+const Spiderman = new Movie("Dune", "Denis Villeneuve", "Action", 2021, 81);
