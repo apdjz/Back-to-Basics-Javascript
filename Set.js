2a6381a953363cffbf084cb0c8a9b2e9d901ce98
Andrew Ziegler <14apdjz@gmail.com>
2022-02-26 15:39:25 -0500

    Create Set.js

---
+++ Set.js
@@ -0,0 +1,11 @@
+let characters = new Set(['a', 'a', 'b', 'c', 'c']);
+console.log(characters);
+
+// add()
+// clear()
+// delete()
+// entries()
+// forEach(callback [, thisArg])
+// has(value)
+// keys()
+// [@@iterator]
